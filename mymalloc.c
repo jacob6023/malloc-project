ecdff11c077ab4d63ef60a2e4fdfe26414751524
MuffinCrow <dbotney@yahoo.com>
2023-10-17 02:19:53 -0400

    Check for size
    Development stopped as I need to either get C fully working on VSCode with make (which make doesn't want to work) or I need to move the code over to the ilabs

--- mymalloc.c
+++ mymalloc.c
@@ -6,7 +6,11 @@
 #include "mymalloc.h"
 
 void* mymalloc(size_t size, char *file, int line) {
-
+    if (size == 0) {
+        printf("%s: %d: Error: Cannot Allocate 0 Bytes.", file, line);
+        return NULL;
+    }
+    return NULL;
 }
 
 void myfree(void *ptr, char *file, int line) {
